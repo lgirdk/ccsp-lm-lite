63d9c78560fe7b94e5e3a1d4a279cc3e39f1c4b7
Avinash Sharma <Avinash.Sharma@cognizant.com>
2017-07-28 10:18:27 +0530

    RPI-44:[LMLite] Device.X_RDKCENTRAL-COM_Report.NetworkDevicesStatus.PollingPeriod not changing to default polling period after the default OverrideTTL period expired. RPI-43:[LMLite] Device.X_RDKCENTRAL-COM_Report.NetworkDevicesStatus.ReportingPeriod not changing to default reporting period after the default OverrideTTL period expired.
    Reason for change: RPi Platform don't have Cable Modem and Cable Modem specific code
    was creating issues on RPi Platform.
    
    Test Procedure: Verified using dmcli commands.
    
    Risks: None
    Signed-off-by: Avinash.Sharma@Cognizant.com
    
    Change-Id: I9ac3da028e194549fc7bd0546acc1d601d3174a5

--- source/lm/webpa_interface.c
+++ source/lm/webpa_interface.c
@@ -494,6 +494,12 @@ char * getDeviceMac()
 {
     CcspLMLiteConsoleTrace(("RDK_LOG_DEBUG, LMLite %s ENTER\n", __FUNCTION__ ));
 
+#if defined(_PLATFORM_RASPBERRYPI_)
+    // Return without performing any operation as RPi Platform don't have Cable Modem and execution
+    // of this function on RPI puts calling thread in infinite wait.
+    return deviceMAC;
+#endif
+
     while(!strlen(deviceMAC))
     {
         pthread_mutex_lock(&device_mac_mutex);
