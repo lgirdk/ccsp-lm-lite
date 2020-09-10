d6c396ddb936250cc98b9582467780a3f7938b19
Andre McCurdy <armccurdy@gmail.com>
2020-09-09 17:49:49 -0700

    fix missing declaration for Send_MoCA_Host_Sync_Req() + make static
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source/lm/lm_main.c
+++ source/lm/lm_main.c
@@ -301,6 +301,9 @@ static void Wifi_ServerSyncHost(char *phyAddr, char *AssociatedDevice, char *ssi
 static void Host_FreeIPAddress(PLmObjectHost pHost, int version);
 static void Hosts_SyncDHCP(void);
 
+#if !defined(_CBR_PRODUCT_REQ_) && !defined(_HUB4_PRODUCT_REQ_)
+static void Send_MoCA_Host_Sync_Req(void);
+#endif
 
 #ifdef USE_NOTIFY_COMPONENT
 
@@ -3359,7 +3362,8 @@ void MoCA_Server_Sync_Function( char *phyAddr, char *AssociatedDevice, char *ssi
 }
 
 
-void Send_MoCA_Host_Sync_Req()
+#if !defined(_CBR_PRODUCT_REQ_) && !defined(_HUB4_PRODUCT_REQ_)
+static void Send_MoCA_Host_Sync_Req(void)
 {
         parameterValStruct_t  value = {"Device.MoCA.X_RDKCENTRAL-COM_MoCAHost_Sync", "true", ccsp_boolean};
         char compo[256] = "eRT.com.cisco.spvtg.ccsp.moca";
@@ -3392,6 +3396,7 @@ void Send_MoCA_Host_Sync_Req()
 		}
 	}
 }
+#endif
 
 void Send_Eth_Host_Sync_Req()
 {
