85cbc56b909522247b8f324141225a1b300296cc
Andre McCurdy <armccurdy@gmail.com>
2020-12-28 18:47:36 -0800

    implement TR table for Friendly Name Implementation ( RDKB-4670 )
    Description:
    Implemented Device.Hosts.Host.{i}.X_LGI-COM_FriendlyName parameter.
    
    TESTING PERFORMED:
    
    1.Connect LAN client to the CM
    2.set the X_LGI-COM_FriendlyName parameter value for the connected lan client
     dmcli eRT setv Device.Hosts.Host.1.X_LGI-COM_FriendlyName string "HomePC"
    3.Reboot the modem and check X_LGI-COM_FriendlyName is retained
    4.Connected another lan client and checked get and set of friendly name param is working properly.
    
    Part 1/2
    
    Signed-off-by: Gokul <gdharmarajan.contractor@libertyglobal.com>

--- source/lm/cosa_hosts_dml.c
+++ source/lm/cosa_hosts_dml.c
@@ -72,6 +72,7 @@
 #include "lm_util.h"
 #include "ctype.h"
 #include <syscfg/syscfg.h>
+#include <sysevent/sysevent.h>
 
 extern LmObjectHosts lmHosts;
 
@@ -1086,7 +1087,8 @@ Host_IsUpdated
     )
 {
     UNREFERENCED_PARAMETER(hInsContext);
-    if ( HostsUpdateTime == 0 ) 
+
+    if ( HostsUpdateTime == 0  || (HostsUpdateTime > AnscGetTickInSeconds()) )
     {
         HostsUpdateTime = AnscGetTickInSeconds();
 
@@ -1103,6 +1105,32 @@ Host_IsUpdated
 
         return TRUE;
     }
+}
+
+#define RESP_STR_LEN        128
+#define SE_WELL_KNOWN_IP    "127.0.0.1"
+
+static BOOL getSysevent(char *event, char *value, int valueLen)
+{
+    int sysevent_fd;
+    token_t sysevent_token = 0;
+
+    value[0] = 0;
+
+    sysevent_fd = sysevent_open(SE_WELL_KNOWN_IP, SE_SERVER_WELL_KNOWN_PORT, SE_VERSION, event, &sysevent_token);
+    if (sysevent_fd < 0)
+    {
+        return FALSE;
+    }
+
+    if (sysevent_get(sysevent_fd, sysevent_token, event, value, valueLen) != 0)
+    {
+        sysevent_close(sysevent_fd, sysevent_token);
+        return FALSE;
+    }
+    sysevent_close(sysevent_fd, sysevent_token);
+
+    return TRUE;
 }
 
 /**********************************************************************  
@@ -1133,10 +1161,26 @@ Host_Synchronize
         ANSC_HANDLE                 hInsContext
     )
 {
+    ULONG count,i;
+    char out[RESP_STR_LEN];
+    BOOL gotEvent = FALSE;
+
     UNREFERENCED_PARAMETER(hInsContext);
+
     //CosaDmlHostsGetHosts(NULL,&count);
 
-	LM_get_host_info();
+    /*
+     * We need to tell LMLite that the parameters request comes from PandMSsp,
+     * otherwise LMLite will request parameters back to PandMSsp,
+     * PandMSsp and LMLite will keep to wait the response from each other and it results deadlock.
+     */
+    gotEvent = getSysevent("get_from_manageable_device", out, sizeof(out));
+    if (((gotEvent == TRUE) && (!AnscEqualString(out, "true", TRUE))) ||
+        (gotEvent == FALSE))
+    {
+        LM_get_host_info();
+    }
+
     HostsUpdateTime = AnscGetTickInSeconds();
 
     return 0;
@@ -1538,6 +1582,21 @@ Host_GetParamStringValue
         }
     }
 
+    /*
+       Almost all parameters should be found within lmHosts.pHostStringParaName[]
+       Handle any special cases which are not.
+    */
+    if ((rc != 0) && (strcmp (ParamName, "X_LGI-COM_FriendlyName") == 0))
+    {
+        rc = 0;
+        if (pHost->pStringParaValue[LM_HOST_PhysAddressId])
+        {
+            snprintf (buf1, sizeof(buf1), "host_friendly_name_%s", pHost->pStringParaValue[LM_HOST_PhysAddressId]);
+            syscfg_get (NULL, buf1, buf2, sizeof(buf2));
+            value = buf2;
+        }
+    }
+
     return GetParamStringValue_common (pValue, pUlSize, value, rc, &LmHostObjectMutex);
 }
 
@@ -1763,6 +1822,36 @@ Host_SetParamStringValue
 			AnscTraceWarning(("<%s> Enter Valid AddressSource [%s] is invalid\n",__FUNCTION__,pString));
 		}
 	}
+	else if (strcmp(ParamName, "X_LGI-COM_FriendlyName") == 0)
+	{
+		char buf1[64];
+
+		if (pHost->pStringParaValue[LM_HOST_PhysAddressId] == NULL)
+		{
+			pthread_mutex_unlock(&LmHostObjectMutex);
+			return TRUE;
+		}
+
+		snprintf(buf1, sizeof(buf1), "host_friendly_name_%s", pHost->pStringParaValue[LM_HOST_PhysAddressId]);
+
+		if (syscfg_set(NULL, buf1, pString) != 0)
+		{
+			pthread_mutex_unlock(&LmHostObjectMutex);
+			return FALSE;
+		}
+
+		if (syscfg_commit() != 0)
+		{
+			CcspTraceWarning(("X_LGI-COM_FriendlyName syscfg_commit failed\n"));
+
+			pthread_mutex_unlock(&LmHostObjectMutex);
+			return FALSE;
+		}
+
+		pthread_mutex_unlock(&LmHostObjectMutex);
+		return TRUE;
+	}
+
 	pthread_mutex_unlock(&LmHostObjectMutex); 
     /* AnscTraceWarning(("Unsupported parameter '%s'\n", ParamName)); */
     return FALSE;
