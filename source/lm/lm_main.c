45dcaa931738d77ddb6911cdd5c433020b12a235
qlei <rlei@libertyglobal.com>
2021-01-11 13:57:40 -0800

    modify LM_HOST_Layer1InterfaceId string format ( OFW-321 )
    Signed-off-by: XXXXX <XXXXX@YYYYY.com>
    
    fix CCSP Device.Ethernet.Interface. entries are not updated ( OFW-504 )
    
    BUG DESCRIPTION:
      - Layer1Interface from Device.Host table is not getting updated when device is re-plugged to different port
      - Device.Ethernet.Interface. entries parameters are not updated. Status, CurrentBitRate and MaxBitRate parameters needs to be updated
    
    FIX DESCRIPTION:
       - Fix has been added to update Layer1Interface parameter from Device.Host table .
       - Fix has been added to update status,CurrentBitRate and MaxBitRate parameter from Device.Ethernet.Interface. entries.
    
    TESTING PERFORMED:
       - Validated Layer1Interface parameter from Device.Host table
       - Validated status,CurrentBitRate and MaxBitRate parameter from Device.Ethernet.Interface. entries
    
    Signed-off-by: Vandana Singh(vasingh.ext@libertyglobal.com)

--- source/lm/lm_main.c
+++ source/lm/lm_main.c
@@ -1147,6 +1147,25 @@ PLmObjectHost XHosts_FindHostByPhysAddress (char * physAddress)
     return NULL;
 }
 
+static void set_Layer1InterfaceId_for_ethernet (LmObjectHost *pHost, unsigned char *mac)
+{
+    char buf[40];
+    char *layer1InterfaceId = "Ethernet";
+    int port = -1;
+
+    if (CcspHalEthSwLocatePortByMacAddress (mac, &port) == RETURN_OK)
+    {
+        /* port 0 is internal? */
+        if (port >= 1)
+        {
+            snprintf (buf, sizeof(buf), "Device.Ethernet.Interface.%d", port);
+            layer1InterfaceId = buf;
+        }
+    }
+
+    LanManager_CheckCloneCopy (&(pHost->pStringParaValue[LM_HOST_Layer1InterfaceId]), layer1InterfaceId);
+}
+
 #define MACADDR_SZ      18
 #define ATOM_MAC        "00:00:ca:01:02:03"
 #define ATOM_MAC_CSC    "00:05:04:03:02:01"
@@ -1292,7 +1311,7 @@ PLmObjectHost Hosts_AddHostByPhysAddress(char *physAddress)
         }
         else
         {
-            pHost->pStringParaValue[LM_HOST_Layer1InterfaceId] = AnscCloneString("Ethernet");
+            set_Layer1InterfaceId_for_ethernet (pHost, physAddress);
         }
 
         pHost->pStringParaValue[LM_HOST_AddressSource] = AnscCloneString("DHCP");
@@ -2242,7 +2261,8 @@ static void *Event_HandlerThread(void *threadid)
 
             if(EthHost.Active)
             {
-                LanManager_CheckCloneCopy(&(pHost->pStringParaValue[LM_HOST_Layer1InterfaceId]), "Ethernet");
+                set_Layer1InterfaceId_for_ethernet (pHost, EthHost.MacAddr);
+
                 if ( ! pHost->pStringParaValue[LM_HOST_IPAddressId] )
                 {
                     CcspTraceWarning(("RDKB_CONNECTED_CLIENTS: Client type is Ethernet, MacAddress is %s IPAddr is not updated in ARP\n",pHost->pStringParaValue[LM_HOST_PhysAddressId]));
