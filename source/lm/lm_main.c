c232d7f01ffde89d2189103ed0cd493f666dcd6c
BijeeshaVS <bvattakaithayilsudha.contractor@libertyglobal.com>
2021-07-08 19:29:04 +0200

    Fix for HostName Value is mac address for LAN client with Invalid Hostname ( OFW-1502 )
    DESCRIPTION:-
    
     HostName value is MAC address for LAN client with Invalid Hostname. For Invalid client name it should show Device.Hosts.Host.1.HostName
     as "Unknown".
    
    TESTING PERFORMED:-
    
    Connected LAN client with invalid hostname and verified that UI is loading connected devices entry with "unknown" as the hostname.

--- source/lm/lm_main.c
+++ source/lm/lm_main.c
@@ -1263,7 +1263,7 @@ static PLmObjectHost XHosts_AddHostByPhysAddress (char *physAddress)
     if (pHost)
     {
         pHost->pStringParaValue[LM_HOST_PhysAddressId] = AnscCloneString(physAddress);
-        pHost->pStringParaValue[LM_HOST_HostNameId] = AnscCloneString(physAddress);
+        pHost->pStringParaValue[LM_HOST_HostNameId] = AnscCloneString("unknown");
 
         comments[0] = 0;
         _getLanHostComments(physAddress, comments);
