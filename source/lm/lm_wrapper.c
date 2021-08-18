3d0068e750b25421a313421c1d3c01a3da27014d
Andre McCurdy <armccurdy@gmail.com>
2021-08-18 00:06:16 -0700

    drop unnecessary local buffers in lm_wrapper_get_arp_entries()
    Also cleanup _get_shell_output() local function and make it static.
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source/lm/lm_wrapper.c
+++ source/lm/lm_wrapper.c
@@ -1054,22 +1054,32 @@ RET1:
     return rVal;
 }
 #endif
-void _get_shell_output(char * cmd, char * out, int len)
+
+#if !defined(INTEL_PUMA7) && !defined(_COSA_BCM_MIPS_) && !defined(_COSA_BCM_ARM_) && !defined(_PLATFORM_TURRIS_)
+/*
+   Note that there are two versions of _get_shell_output() used with RDKB.
+   This version, which accepts a char * command as the first argument, is
+   the older version. The newer version accepts a FILE pointer as created
+   by a call to v_secure_popen().
+*/
+static void _get_shell_output (char *cmd, char *buf, size_t len)
 {
-    FILE * fp;
-    char   buf[256] = {0};
-    char * p;
-    fp = popen(cmd, "r");
-    if (fp)
-    {
-        fgets(buf, sizeof(buf), fp);
-        /*we need to remove the \n char in buf*/
-        if ((p = strchr(buf, '\n'))) *p = 0;
-        strncpy(out, buf, len-1);
-        pclose(fp);        
+    FILE *fp;
+
+    if (len > 0)
+        buf[0] = 0;
+    fp = popen (cmd, "r");
+    if (fp == NULL)
+        return;
+    buf = fgets (buf, len, fp);
+    pclose (fp);
+    if ((len > 0) && (buf != NULL)) {
+        len = strlen (buf);
+        if ((len > 0) && (buf[len - 1] == '\n'))
+            buf[len - 1] = 0;
     }
 }
-
+#endif
 
 int lm_wrapper_get_arp_entries (char netName[LM_NETWORK_NAME_SIZE], int *pCount, LM_host_entry_t **ppArray)
 {
@@ -1085,19 +1095,16 @@ int lm_wrapper_get_arp_entries (char netName[LM_NETWORK_NAME_SIZE], int *pCount,
     unlink(ARP_CACHE_FILE);
    // snprintf(buf, sizeof(buf), "ip nei show | grep %s | grep -v 192.168.10 > %s", netName, ARP_CACHE_FILE);
 
-	// XB6/XF3 Do not have this interface. Remove constant warnings.
+    // XB6/XF3 Do not have this interface. Remove constant warnings.
 #if !defined(INTEL_PUMA7) && !defined(_COSA_BCM_MIPS_) && !defined(_COSA_BCM_ARM_) && !defined(_PLATFORM_TURRIS_)
     // This is added to remove atom mac from the connected device list.
-    char cmd[256] = {0};
-    char out[32] = {0};
-     if(pAtomBRMac[0] == '\0' || pAtomBRMac[0] == ' ') {
-		_ansc_sprintf(cmd, "ifconfig l2sd0 | grep HWaddr | awk '{print $5}' | cut -c 1-14\n" );
-		_get_shell_output(cmd, out, sizeof(out));
-		 strncpy(pAtomBRMac,out,sizeof(out));
-		CcspTraceWarning(("Atom mac is %s \n",pAtomBRMac));
-   	}
+    if (pAtomBRMac[0] == '\0' || pAtomBRMac[0] == ' ') {
+        char *cmd = "ifconfig l2sd0 | grep HWaddr | awk '{print $5}' | cut -c 1-14";
+        _get_shell_output(cmd, pAtomBRMac, sizeof(pAtomBRMac));
+        CcspTraceWarning(("Atom mac is %s\n",pAtomBRMac));
+    }
 #endif
- 
+
     if(pAtomBRMac[0] != '\0'  &&  pAtomBRMac[0] != ' ') {
     	v_secure_system("ip -4 nei show | grep %s | grep -v 192.168.10  | grep -i -v %s > "ARP_CACHE_FILE, netName,pAtomBRMac);
         v_secure_system("ip -6 nei show | grep %s | grep -i -v %s >> "ARP_CACHE_FILE, netName, pAtomBRMac);
