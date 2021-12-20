c7af472fb1899a5d419d75930a3255e2f2a41dad
Mustafa Karadzha <mkaradzha@libertyglobal.com>
2021-12-20 16:09:47 +0100

    restructure code to speed up Hosts table queries ( OFW-2201 CH7465SI-103 )
    [PERFORMANCE] CcspLMLite process takes more cpu time.
    
    [Issue]
    "CcspHalEthSwLocatePortByMacAddress()" calls are taking too much time.
    
    [Solution]
    LMLite queries the switch port for each LAN MAC address while the host table mutex is held.
    
    Restructuring code to move the port queries out of the locked context to prevent hosts
    table queries from being blocked for the full duration of switch port lookups.
    
    [Testing Performed]
    "Device.Hosts." table queries are observed to be faster by checking the duration on ATOM console:
    
      while true; do time dmcli eRT getv Device.Hosts.Host. > /dev/null; done
    
    Signed-off-by: Mustafa Karadzha(mkaradzha@libertyglobal.com)

--- source/lm/lm_wrapper.c
+++ source/lm/lm_wrapper.c
@@ -1498,7 +1498,7 @@ void lm_wrapper_get_dhcpv4_client()
         {
             if(! (pAtomBRMac[0] != '\0'  &&  pAtomBRMac[0] != ' ' && strcasestr((const char *)dhcpHost.phyAddr,pAtomBRMac) != NULL ))
             {
-                pHost = Hosts_AddHostByPhysAddress((char *)dhcpHost.phyAddr);
+                pHost = Hosts_AddHostByPhysAddress((char *)dhcpHost.phyAddr, -1);
 
                 if ( pHost )
                 {
@@ -1599,7 +1599,7 @@ void lm_wrapper_get_dhcpv4_reserved()
 
         if ( !pHost )
         {
-            pHost = Hosts_AddHostByPhysAddress((char *)dhcpHost.phyAddr);
+            pHost = Hosts_AddHostByPhysAddress((char *)dhcpHost.phyAddr, -1);
 
             if ( pHost )
             {
